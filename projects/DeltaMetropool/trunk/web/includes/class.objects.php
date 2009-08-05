c20a4bb4bf976f964cabf5279f61e29ecb4f8de6
gstolk <gstolk@c8b22b7e-d564-0410-9065-c9e2a09a08ec>
2009-08-05 06:55:30 +0000

    Bring the simple php framework vendor drop of tylerhall of 29 juli 2009 into trunk

---
+++ projects/DeltaMetropool/trunk/web/includes/class.objects.php
@@ -0,0 +1,11 @@
+<?PHP
+    // Stick your DBOjbect subclasses in here (to help keep things tidy).
+
+    class User extends DBObject
+    {
+        public function __construct($id = null)
+        {
+            parent::__construct('users', array('username', 'password', 'level', 'email'), $id);
+        }
+    }
+
