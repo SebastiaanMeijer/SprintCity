f94ff3fb8e20b07e08fe0f8592c17ffd1039dd2c
gstolk <gstolk@c8b22b7e-d564-0410-9065-c9e2a09a08ec>
2009-08-11 15:00:43 +0000

    Added constants edit page.

--- projects/DeltaMetropool/trunk/web/pages/submit_form.php
+++ projects/DeltaMetropool/trunk/web/pages/submit_form.php
@@ -8,6 +8,9 @@ switch( $_REQUEST['Action'] )
 	case "new_team":
 		NewTeam();
 		break;
+	case "edit_constants":
+		EditConstants();
+		break;
 	default:
 }
 
@@ -26,6 +29,21 @@ function NewTeam()
 		'created' => date( 'Y-m-d H:i:s'));
 	$db->query($query, $args);
 	header("Location: ../admin.php?view=teams&page=" . isset($_REQUEST['page']) ? $_REQUEST['page'] : 1);
+}
+
+function EditConstants()
+{
+	$db = Database::getDatabase();	
+	$query = "
+		UPDATE `constants` 
+		SET `average_citizens_per_home` = :average_citizens_per_home, 
+			`average_workers_per_bvo` = :average_workers_per_bvo 
+		LIMIT 1;";
+	$args = array(
+		'average_citizens_per_home' => $_REQUEST['average_citizens_per_home'], 
+		'average_workers_per_bvo' => $_REQUEST['average_workers_per_bvo']);
+	$db->query($query, $args);
+	header("Location: ../admin.php?view=constants");
 }
 
 ?>
