fd3e2e173b3c3311a84a6ed90f552b6cb80b49b2
gstolk <gstolk@c8b22b7e-d564-0410-9065-c9e2a09a08ec>
2009-08-14 07:20:13 +0000

    Added 'new game' back end functionality. Fixed a bug in the stylesheet.

--- projects/DeltaMetropool/trunk/web/includes/class.objects.php
+++ projects/DeltaMetropool/trunk/web/includes/class.objects.php
@@ -1,22 +1,22 @@
 <?PHP
-    // Stick your DBOjbect subclasses in here (to help keep things tidy).
+	// Stick your DBOjbect subclasses in here (to help keep things tidy).
 
-    class User extends DBObject
-    {
-        public function __construct($id = null)
-        {
-            parent::__construct('users', array('username', 'password', 'level', 'email'), $id);
-        }
-    }
-    
-    class Team extends DBObject
-    {
-    	public function __construct($id = null)
-    	{
-    		parent::__construct('Team', array('name', 'description', 'cpu', 'created'), $id);
-    	}
-    	
-    	public static function rowCount()
+	class User extends DBObject
+	{
+		public function __construct($id = null)
+		{
+			parent::__construct('users', array('username', 'password', 'level', 'email'), $id);
+		}
+	}
+	
+	class Team extends DBObject
+	{
+		public function __construct($id = null)
+		{
+			parent::__construct('Team', array('name', 'description', 'cpu', 'created'), $id);
+		}
+		
+		public static function rowCount()
 		{
 			$db = Database::getDatabase();
 			return $db->getValue("SELECT COUNT(*) FROM team");
@@ -24,7 +24,32 @@
 		
 		public static function getTeams($fromIndex, $numberOfRecords)
 		{
-			return DBObject::glob("Team", "SELECT * FROM  `team` ORDER BY  `created` DESC LIMIT " . $fromIndex . " , " . $numberOfRecords);
+			return DBObject::glob("Team", "SELECT * FROM  `team` ORDER BY `created` DESC LIMIT " . $fromIndex . " , " . $numberOfRecords);
 		}
-    }
+	}
+	
+	class Station extends DBObject
+	{
+		public function __construct($id = null)
+		{
+			parent::__construct('Station', array(
+				'code', 'name', 'description', 'image', 'town', 'region', 
+				'POVN', 'PWN', 'IWD', 'MNG', 
+				'area_cultivated_home', 'area_cultivated_work', 'area_cultivated_mixed', 'area_undeveloped_urban', 'area_undeveloped_rural',
+				'transform_area_cultivated_home', 'transform_area_cultivated_work', 'transform_area_cultivated_mixed', 'transform_area_undeveloped_urban', 'transform_area_undeveloped_rural', 
+				'count_home_total', 'count_home_transform', 'count_work_total', 'count_work_transform', 'network_value'), 
+				$id);
+		}
+		
+		public static function rowCount()
+		{
+			$db = Database::getDatabase();
+			return $db->getValue("SELECT COUNT(*) FROM station");
+		}
+		
+		public static function getStations($fromIndex, $numberOfRecords)
+		{
+			return DBObject::glob("Station", "SELECT * FROM  `station` ORDER BY `code` ASC LIMIT " . $fromIndex . " , " . $numberOfRecords);
+		}
+	}
 
--- projects/DeltaMetropool/trunk/web/pages/submit_form.php
+++ projects/DeltaMetropool/trunk/web/pages/submit_form.php
@@ -5,6 +5,9 @@ require_once '../includes/master.inc.php';
 
 switch( $_REQUEST['Action'] )
 {
+	case "new_game":
+		NewGame();
+		break;
 	case "new_team":
 		NewTeam();
 		break;
@@ -14,9 +17,47 @@ switch( $_REQUEST['Action'] )
 	default:
 }
 
+function NewGame()
+{
+	$db = Database::getDatabase();
+	
+	$query = "
+		INSERT INTO `Game` 
+			(`name` , `notes` , `starttime`)
+		VALUES 
+			(:name, :notes, :starttime);";
+	$args = array(
+		'name' => $_REQUEST['name'], 
+		'notes' => $_REQUEST['notes'], 
+		'starttime' => date( 'Y-m-d H:i:s'));
+	$db->query($query, $args);
+	
+	$game_id = mysql_insert_id($db->db);
+	$stations = Station::getStations(0, Station::rowCount());
+	foreach ($stations as $station_key => $station_value) 
+	{
+		if (isset($_REQUEST['team_' . $station_key]) &&
+			$_REQUEST['team_'. $station_key] != "null")
+		{
+			$query = "
+				INSERT INTO `StationInstance` 
+					(`station_id` , `team_id` , `game_id`)
+				VALUES 
+					(:station_id, :team_id, :game_id);";
+			$args = array(
+				'station_id' => $station_key, 
+				'team_id' => $_REQUEST['team_' . $station_key], 
+				'game_id' => $game_id);
+			$db->query($query, $args);
+		}
+	}
+	header("Location: ../admin.php?view=games");
+}
+
 function NewTeam()
 {
 	$db = Database::getDatabase();	
+	
 	$query = "
 		INSERT INTO `team` 
 			(`name` , `description` , `cpu`, `created`)
@@ -28,12 +69,14 @@ function NewTeam()
 		'cpu' => isset($_REQUEST['cpu']),
 		'created' => date( 'Y-m-d H:i:s'));
 	$db->query($query, $args);
-	header("Location: ../admin.php?view=teams&page=" . isset($_REQUEST['page']) ? $_REQUEST['page'] : 1);
+	
+	header("Location: ../admin.php?view=teams&page=" . (isset($_REQUEST['page']) ? $_REQUEST['page'] : 1));
 }
 
 function EditConstants()
 {
 	$db = Database::getDatabase();	
+	
 	$query = "
 		UPDATE `constants` 
 		SET `average_citizens_per_home` = :average_citizens_per_home, 
@@ -43,7 +86,7 @@ function EditConstants()
 		'average_citizens_per_home' => $_REQUEST['average_citizens_per_home'], 
 		'average_workers_per_bvo' => $_REQUEST['average_workers_per_bvo']);
 	$db->query($query, $args);
+	
 	header("Location: ../admin.php?view=constants");
 }
-
 ?>
