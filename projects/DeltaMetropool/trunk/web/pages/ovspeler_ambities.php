b50f2ec740435b44dd51f0be5f04621fc1fa95f9
fpvanagthoven <fpvanagthoven@c8b22b7e-d564-0410-9065-c9e2a09a08ec>
2010-10-15 15:53:52 +0000

    Inputfields, little bit of css

---
+++ projects/DeltaMetropool/trunk/web/pages/ovspeler_ambities.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<meta name="keywords" content="">
+<meta name="description" content="">
+<title>OV-Speler Scherm</title>
+<link rel="stylesheet" type="text/css" href="../style/ovspeler.css">
+<link rel="stylesheet" type="text/css" href="../style/ovspelerambities.css">
+</head>
+
+<body>
+
+<div id="left">
+</div>
+<div id="main">
+</div>
+<div id="right">
+</div>
+
+</body>
+
+</html>
