cf0eb331e570b568d07447619f68589f760e0999
fpvanagthoven <fpvanagthoven@c8b22b7e-d564-0410-9065-c9e2a09a08ec>
2010-10-22 09:38:24 +0000

    IFrame inserted so that flash app does not reload.

---
+++ projects/DeltaMetropool/trunk/web/mobilityheader.php
@@ -0,0 +1,11 @@
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+		<meta name="keywords" content="">
+		<meta name="description" content="">
+		<title>NS</title>
+		<link href="images/mobility/ns-logo.ico" rel="shortcut icon"/> 
+		<link rel="stylesheet" type="text/css" href="style/reset-fonts-grids.css">
+		<link rel="stylesheet" type="text/css" href="style/mobility.css">
+		<script type="text/javascript" src="script/mobility/resize.js"></script>
+		<script type="text/javascript" src="script/mobility/checkMax.js"></script>
+	</head>
