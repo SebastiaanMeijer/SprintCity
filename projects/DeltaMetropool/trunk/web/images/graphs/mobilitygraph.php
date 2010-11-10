e04ce1376aea5b0f7c13a99dacb9111b59403ae8
jkraaijeveld <jkraaijeveld@c8b22b7e-d564-0410-9065-c9e2a09a08ec>
2010-11-10 13:26:12 +0000

    Space and mobility graphs now use the minimum and maximum values of all stations as axis values.

--- projects/DeltaMetropool/trunk/web/images/graphs/mobilitygraph.php
+++ projects/DeltaMetropool/trunk/web/images/graphs/mobilitygraph.php
@@ -30,8 +30,8 @@ if (ClientSession::hasSession(session_id()))
 	$graph = new LineGraph($width, $height);
 	
 	// Set input
-	$graph->SetInputArray($povnData);
-	$graph->SetInputArray($travelerData);
+	$graph->SetInputArray($povnData, loadPOVNDataMin($gameId), loadPOVNDataMax($gameId));
+	$graph->SetInputArray($travelerData, loadTravelerDataMin($gameId), loadTravelerDataMax($gameId));
 	$graph->SetToMobilityColors();
 	
 	// Get image
@@ -45,6 +45,244 @@ if (ClientSession::hasSession(session_id()))
 	
 	// Destroy garbage
 	imagedestroy($image);
+}
+
+function loadPOVNDataMin($game_id)
+{
+	if (isset($game_id))
+	{
+		$db = Database::getDatabase();
+		$query = 	"
+					SELECT MIN(POVN)
+					FROM(
+						SELECT RoundInstance.POVN FROM RoundInstance
+						INNER JOIN StationInstance ON RoundInstance.station_instance_id = StationInstance.id
+						INNER JOIN TeamInstance ON StationInstance.team_instance_id = TeamInstance.id
+						INNER JOIN Game ON TeamInstance.game_id = Game.id
+						INNER JOIN Round ON RoundInstance.round_id = Round.id
+						WHERE Game.id = :game_id 
+						AND Round.round_info_id < Game.current_round_id) AS t1;
+				";
+		$args = array('game_id' => $game_id);
+		$result = $db->query($query, $args);
+		return $db->getValue($result);
+	}
+}
+
+function loadPOVNDataMax($game_id)
+{
+	if (isset($game_id))
+	{
+		$db = Database::getDatabase();
+		$query = 	"
+					SELECT MAX(POVN)
+					FROM(
+						SELECT RoundInstance.POVN FROM RoundInstance
+						INNER JOIN StationInstance ON RoundInstance.station_instance_id = StationInstance.id
+						INNER JOIN TeamInstance ON StationInstance.team_instance_id = TeamInstance.id
+						INNER JOIN Game ON TeamInstance.game_id = Game.id
+						INNER JOIN Round ON RoundInstance.round_id = Round.id
+						WHERE Game.id = :game_id 
+						AND Round.round_info_id < Game.current_round_id) AS t1;
+				";
+		$args = array('game_id' => $game_id);
+		$result = $db->query($query, $args);
+		return $db->getValue($result);
+	}
+}
+
+function loadTravelerDataMin($game_id)
+{
+	if (isset($game_id))
+	{
+		$db = Database::getDatabase();
+		$query = "
+			SELECT MIN(TravelerCount)
+			FROM( SELECT ROUND
+					(
+						(
+							(
+								(
+									Station.area_cultivated_mixed - 
+									(
+										(SUM(Program.area_home) + SUM(Program.area_work) + SUM(Program.area_leisure)) 
+										* 
+										(transform_area_cultivated_mixed / (transform_area_cultivated_home + transform_area_cultivated_work + transform_area_cultivated_mixed + transform_area_undeveloped_urban + transform_area_undeveloped_mixed))
+									)
+								) 
+								* 
+								Constants.average_travelers_per_ha_leisure 
+							)
+							+
+							(
+								(
+									(
+										(
+											Station.area_cultivated_home - 
+											(
+												(SUM(Program.area_home) + SUM(Program.area_work) + SUM(Program.area_leisure)) 
+												* 
+												(transform_area_cultivated_home / (transform_area_cultivated_home + transform_area_cultivated_work + transform_area_cultivated_mixed + transform_area_undeveloped_urban + transform_area_undeveloped_mixed))
+											)
+										)
+										* 
+										(count_home_total / area_cultivated_home)
+									) 
+									+ 
+									SUM(Program.area_home * TypesHome.density)
+								) 
+								* 
+								Constants.average_citizens_per_home * Constants.average_travelers_per_citizen
+							) 
+							+
+							(
+								(
+									(
+										(
+											Station.area_cultivated_work - 
+											(
+												(SUM(Program.area_home) + SUM(Program.area_work) + SUM(Program.area_leisure)) 
+												* 
+												(transform_area_cultivated_work / (transform_area_cultivated_home + transform_area_cultivated_work + transform_area_cultivated_mixed + transform_area_undeveloped_urban + transform_area_undeveloped_mixed))
+											)
+										)
+										* 
+										(count_work_total / area_cultivated_work)
+									) 
+									+ 
+									SUM(Program.area_work * TypesWork.density)
+								) 
+								* 
+								Constants.average_workers_per_bvo * Constants.average_travelers_per_worker
+							)
+						)
+						*
+						(
+							(RoundInstance2.POVN - Station.POVN) 
+							/ 
+							Station.POVN 
+							/
+							IF((RoundInstance2.POVN - Station.POVN) / Station.POVN > 5, 20, IF((RoundInstance2.POVN - Station.POVN) / Station.POVN > 1, 15, 10))
+							+ 1
+						)
+					) AS TravelerCount
+					FROM Constants, Station
+					INNER JOIN StationInstance ON Station.id = StationInstance.station_id 
+					INNER JOIN TeamInstance ON StationInstance.team_instance_id = TeamInstance.id
+					INNER JOIN RoundInstance ON StationInstance.id = RoundInstance.station_instance_id
+					INNER JOIN Program ON RoundInstance.exec_program_id = Program.id
+					INNER JOIN Types AS TypesHome ON Program.type_home = TypesHome.id
+					INNER JOIN Types AS TypesWork ON Program.type_work = TypesWork.id
+					INNER JOIN Round ON RoundInstance.round_id = Round.id AND Station.id = Round.station_id
+					INNER JOIN RoundInfo ON Round.round_info_id = RoundInfo.id
+					INNER JOIN RoundInfo AS RoundInfo2 ON RoundInfo.id < RoundInfo2.id
+					INNER JOIN Round AS Round2 ON RoundInfo2.id = Round2.round_info_id AND Station.id = Round2.station_id
+					INNER JOIN RoundInstance AS RoundInstance2 ON Round2.id = RoundInstance2.round_id AND StationInstance.id = RoundInstance2.station_instance_id
+					INNER JOIN Game ON TeamInstance.game_id = Game.id AND RoundInfo2.id <= current_round_id
+					WHERE Game.id = :game_id
+					GROUP BY Station.id, RoundInfo2.id
+					ORDER BY RoundInfo2.id) AS t1;";
+		$args = array('game_id' => $game_id);
+		$result = $db->query($query, $args);
+		return $db->getValue($result);
+	}
+}
+
+function loadTravelerDataMax($game_id)
+{
+if (isset($game_id))
+	{
+		$db = Database::getDatabase();
+		$query = "
+			SELECT MAX(TravelerCount)
+			FROM( SELECT ROUND
+					(
+						(
+							(
+								(
+									Station.area_cultivated_mixed - 
+									(
+										(SUM(Program.area_home) + SUM(Program.area_work) + SUM(Program.area_leisure)) 
+										* 
+										(transform_area_cultivated_mixed / (transform_area_cultivated_home + transform_area_cultivated_work + transform_area_cultivated_mixed + transform_area_undeveloped_urban + transform_area_undeveloped_mixed))
+									)
+								) 
+								* 
+								Constants.average_travelers_per_ha_leisure 
+							)
+							+
+							(
+								(
+									(
+										(
+											Station.area_cultivated_home - 
+											(
+												(SUM(Program.area_home) + SUM(Program.area_work) + SUM(Program.area_leisure)) 
+												* 
+												(transform_area_cultivated_home / (transform_area_cultivated_home + transform_area_cultivated_work + transform_area_cultivated_mixed + transform_area_undeveloped_urban + transform_area_undeveloped_mixed))
+											)
+										)
+										* 
+										(count_home_total / area_cultivated_home)
+									) 
+									+ 
+									SUM(Program.area_home * TypesHome.density)
+								) 
+								* 
+								Constants.average_citizens_per_home * Constants.average_travelers_per_citizen
+							) 
+							+
+							(
+								(
+									(
+										(
+											Station.area_cultivated_work - 
+											(
+												(SUM(Program.area_home) + SUM(Program.area_work) + SUM(Program.area_leisure)) 
+												* 
+												(transform_area_cultivated_work / (transform_area_cultivated_home + transform_area_cultivated_work + transform_area_cultivated_mixed + transform_area_undeveloped_urban + transform_area_undeveloped_mixed))
+											)
+										)
+										* 
+										(count_work_total / area_cultivated_work)
+									) 
+									+ 
+									SUM(Program.area_work * TypesWork.density)
+								) 
+								* 
+								Constants.average_workers_per_bvo * Constants.average_travelers_per_worker
+							)
+						)
+						*
+						(
+							(RoundInstance2.POVN - Station.POVN) 
+							/ 
+							Station.POVN 
+							/
+							IF((RoundInstance2.POVN - Station.POVN) / Station.POVN > 5, 20, IF((RoundInstance2.POVN - Station.POVN) / Station.POVN > 1, 15, 10))
+							+ 1
+						)
+					) AS TravelerCount
+					FROM Constants, Station
+					INNER JOIN StationInstance ON Station.id = StationInstance.station_id 
+					INNER JOIN TeamInstance ON StationInstance.team_instance_id = TeamInstance.id
+					INNER JOIN RoundInstance ON StationInstance.id = RoundInstance.station_instance_id
+					INNER JOIN Program ON RoundInstance.exec_program_id = Program.id
+					INNER JOIN Types AS TypesHome ON Program.type_home = TypesHome.id
+					INNER JOIN Types AS TypesWork ON Program.type_work = TypesWork.id
+					INNER JOIN Round ON RoundInstance.round_id = Round.id AND Station.id = Round.station_id
+					INNER JOIN RoundInfo ON Round.round_info_id = RoundInfo.id
+					INNER JOIN RoundInfo AS RoundInfo2 ON RoundInfo.id < RoundInfo2.id
+					INNER JOIN Round AS Round2 ON RoundInfo2.id = Round2.round_info_id AND Station.id = Round2.station_id
+					INNER JOIN RoundInstance AS RoundInstance2 ON Round2.id = RoundInstance2.round_id AND StationInstance.id = RoundInstance2.station_instance_id
+					INNER JOIN Game ON TeamInstance.game_id = Game.id AND RoundInfo2.id <= current_round_id
+					WHERE Game.id = :game_id
+					GROUP BY Station.id, RoundInfo2.id
+					ORDER BY RoundInfo2.id) AS t1;";
+		$args = array('game_id' => $game_id);
+		$result = $db->query($query, $args);
+		return $db->getValue($result);
+	}
 }
 
 function LoadPOVNData($session_id, $station_id)
