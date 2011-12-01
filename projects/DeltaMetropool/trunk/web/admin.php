f03a058c1e10625797d56d66a72141d402ea094f
gstolk <gstolk@c8b22b7e-d564-0410-9065-c9e2a09a08ec>
2011-12-01 09:11:59 +0000

    Added jquery script files locally, typically SprintStad isn't used in combination with an internet connection.

--- projects/DeltaMetropool/trunk/web/admin.php
+++ projects/DeltaMetropool/trunk/web/admin.php
@@ -15,8 +15,8 @@
 <link rel="stylesheet" type="text/css" href="style/base.css">
 <link rel="stylesheet" type="text/css" href="style/style.css">
 <script type="text/javascript" src="script/jscolor/jscolor.js"></script>
-<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js"></script>
-<script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.16/jquery-ui.min.js" type="text/javascript"></script>
+<script type="text/javascript" src="script/jquery/jquery.min.js"></script>
+<script type="text/javascript" src="script/jquery/jquery-ui.min.js" ></script>
 
 </head>
 <body>
