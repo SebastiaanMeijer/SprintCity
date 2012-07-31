acffb425a7b0c3e664ac4d71d58c0f0a2bb2ab6f
fpvanagthoven <fpvanagthoven@c8b22b7e-d564-0410-9065-c9e2a09a08ec>
2012-07-31 18:55:44 +0000

    Readonly state toegevoegd

--- projects/DeltaMetropool/trunk/web/pages/ovapp.php
+++ projects/DeltaMetropool/trunk/web/pages/ovapp.php
@@ -7,6 +7,7 @@
 
     /* ========================================================= */
     /* Initialization */
+    var READ_ONLY = false;
 
     var stations = new Array();
     var trains = new Array();
@@ -22,47 +23,52 @@
         for (var i = 0; i < trains.length; i++) {
             new Traject(i);
         };
-        $('.train-stop').click(function(){
-            handleTrainStopClick(this);
-        });
         
-        $('.traject-title').click(function(){
-            handleTrainClick(this);
-        });
-});
-    
-function handleTrainStopClick(trainStop) {        
-    var stopNum = $(trainStop).text();
-    if (stopNum == 0) {
-        $(trainStop).removeClass('invisible');
-    }
-    if (stopNum < 7){
-        stopNum++;
-        $(trainStop).text(stopNum);
-    }
-    else {
-        stopNum = 0;
-        $(trainStop).addClass('invisible');
-        $(trainStop).text("");
-    }
+        if (!READ_ONLY){
+            $('.train-stop').click(function(){
+                handleTrainStopClick(this);
+            });
         
-    /* prognose */
-}
-    
-function handleTrainClick(trainTitle) {
-    /* Go inside container div and look for all divs with class train-stop
-     * and fire handleTrainStopClick for each one.
-     */
-    $(trainTitle).parent().children('.traject-lijn').children('.train-stop').each(function() {
-        $(this).animate({color: 'black'}, 40, "swing", function(){
-            $(this).animate({color: '#f0098d'}, 40, "linear");
-        });
-        handleTrainStopClick(this);
-            
+            $('.traject-title').click(function(){
+                handleTrainClick(this);
+            });
+        }
     });
+    
+    function handleTrainStopClick(trainStop) {        
+        var stopNum = $(trainStop).text();
+        if (stopNum == 0) {
+            $(trainStop).removeClass('invisible');
+        }
+        if (stopNum < 7){
+            stopNum++;
+            $(trainStop).text(stopNum);
+        }
+        else {
+            stopNum = 0;
+            $(trainStop).addClass('invisible');
+            $(trainStop).text("");
+        }
+        
+        /* prognose */
+    }
+    
+    function handleTrainClick(trainTitle) {
+        /* Go inside container div and look for all divs with class train-stop
+         * and fire handleTrainStopClick for each one.
+         */
+        
+        $(trainTitle).parent().children('.traject-lijn').children('.train-stop').each(function() {
+
+            $(this).animate({color: 'black'}, 20, "swing", function(){
+                $(this).animate({color: '#f0098d'}, 20, "swing");
+            });
+            handleTrainStopClick(this);
+            
+        });
         
         
-}
+    }
 </script>
 <!-- HTML STUFF -->
 
