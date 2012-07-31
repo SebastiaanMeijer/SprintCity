c26b6e5b14c4dd8db66a5b39dd00fc977285d298
fpvanagthoven <fpvanagthoven@c8b22b7e-d564-0410-9065-c9e2a09a08ec>
2012-07-31 12:36:08 +0000

    Fixed station tag name so that it splits properly

--- projects/DeltaMetropool/trunk/web/script/mobility/station.js
+++ projects/DeltaMetropool/trunk/web/script/mobility/station.js
@@ -24,12 +24,16 @@ Station.initMockStations = function(stationArray) {
     stationArray.push(new Station("Den Haag CS", 10, 60, 123));
     stationArray.push(new Station("Den Haag HS", 30, 140, 10));
     stationArray.push(new Station("Den Haag Moerwijk", 20, 50, 60));
-    stationArray.push(new Station("Rijswijk", 4220, 60, 90));
+    stationArray.push(new Station("Rijswijk", 20, 60, 90));
     stationArray.push(new Station("Delft", 12, 200, 180));
     stationArray.push(new Station("Delft zuid", 134, 20, 40));
     stationArray.push(new Station("Schiedam Kethel", 14, 40, 50));
     stationArray.push(new Station("Schiedam Centraal", 40, 10, 170));
     stationArray.push(new Station("Rotterdam Centraal", 70, 200, 50));
+        
+    stationArray.push(new Station("Test1", 10, 60, 123));
+    stationArray.push(new Station("Test2", 30, 140, 10));
+    stationArray.push(new Station("Test3", 20, 50, 60));
         
     return stationArray;
 }
