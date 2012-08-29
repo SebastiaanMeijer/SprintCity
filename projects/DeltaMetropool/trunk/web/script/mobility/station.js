de77c37431525454a3efa8d1585b24b8a6aceeca
fpvanagthoven <fpvanagthoven@c8b22b7e-d564-0410-9065-c9e2a09a08ec>
2012-08-29 13:43:50 +0000

    Click reloads and refreshes the graph.

--- projects/DeltaMetropool/trunk/web/script/mobility/station.js
+++ projects/DeltaMetropool/trunk/web/script/mobility/station.js
@@ -1,14 +1,23 @@
-function Station(name, networkValue, currentIU, cap100) {
+function Station(name, networkValue, prevIU, currentIU, progIU, cap100, capOver, capUnder) {
     this.name = name;
     this.networkValue = networkValue;
 
-    this.prevIU = 50;
+    this.prevIU = prevIU;
     this.currentIU = currentIU;
-    this.progIU = 100;
+    this.progIU = progIU;
 
     this.cap100 = cap100;
-    this.capOver = this.cap100 * 1.25;
-    this.capUnder = this.cap100 * 0.75;
+    
+    if (capOver != -1 && capUnder != -1) {
+        this.capOver = capOver;    
+        this.capUnder = capUnder;
+    }
+    else {
+        this.capOver = this.cap100 * 1.25;
+        this.capUnder = this.cap100 * 0.75;
+    }
+    
+    
 }
 
 
@@ -19,3 +28,37 @@ Station.prototype.setCurrentIU = function(newIU) {
 /* TODO: prognose here */
 };
 
+Station.fillStations = function(data) {
+    for (i = 0; i < data.length; i++) {     
+            
+        stations.push(
+            new Station(
+                data[i].name, 
+                data[i].networkValue,
+                data[i].prevIU,
+                data[i].currentIU,
+                data[i].progIU,
+                data[i].cap100,
+                data[i].capOver,
+                data[i].capUnder
+            ));  
+            
+    }
+}
+
+/* public callback function */
+fillStationsAndDraw = function(data) {    
+    
+    Station.fillStations(data);
+    Graph.drawGraph();
+    
+}
+
+
+
+/* static function */
+Station.refreshStations = function() {
+
+    stations = new Array();
+    Load.loadStations(fillStationsAndDraw);
+}
