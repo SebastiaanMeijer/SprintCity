dadd9542d52d8eb96bb02f279abcac1dd79be885
fpvanagthoven <fpvanagthoven@c8b22b7e-d564-0410-9065-c9e2a09a08ec>
2012-08-31 14:48:08 +0000

    Ambition

---
+++ projects/DeltaMetropool/trunk/web/pages/mobility/mobility_sendvalue.php
@@ -0,0 +1,42 @@
+<?php
+
+require_once '../../includes/master.inc.php';
+
+
+$db = Database::getDatabase();
+$game_id = Game::getGameIdOfSession(session_id());
+// uncheck all ambitions for the mobility player
+$query = "
+			UPDATE ValueInstance
+			INNER JOIN TeamInstance ON ValueInstance.team_instance_id = TeamInstance.id
+			INNER JOIN Team ON TeamInstance.team_id = Team.id
+			SET ValueInstance.checked = false
+			WHERE TeamInstance.game_id = :game_id
+				AND Team.id = :team_id";
+$args = array(
+    'game_id' => $game_id,
+    'team_id' => MOBILITY_TEAM_ID);
+$db->query($query, $args);
+
+// check selected ambitions
+$valueInstanceId = $_REQUEST['valueInstanceId'];
+$query = "
+				UPDATE ValueInstance
+				SET ValueInstance.checked = true
+				WHERE ValueInstance.id = :value_instance_id";
+$args = array('value_instance_id' => $valueInstanceId);
+$db->query($query, $args);
+
+
+//    // fill ambition motivation
+//    $query = "
+//			UPDATE TeamInstance
+//			SET TeamInstance.value_description = :motivation
+//			WHERE TeamInstance.game_id = :game_id
+//				AND TeamInstance.team_id = :team_id";
+//    $args = array(
+//        'motivation' => $_POST['ambitionMotivation'],
+//        'game_id' => $game_id,
+//        'team_id' => MOBILITY_TEAM_ID);
+//    $db->query($query, $args);
+?>
