501aca0d510cb6a35d77963669d2ae340c31bdc2
gstolk <gstolk@c8b22b7e-d564-0410-9065-c9e2a09a08ec>
2012-09-18 09:32:02 +0000

    Added a warning when the imported excel timetable exceeds server restricted maximum upload file size.

--- projects/DeltaMetropool/trunk/web/pages/traintable/edit.php
+++ projects/DeltaMetropool/trunk/web/pages/traintable/edit.php
@@ -4,12 +4,12 @@
 ?>
 						<tr>
 							<td>
-								<form action="./admin.php?view=traintable" enctype="multipart/form-data" method="POST">
+								<form action="./admin.php?view=traintable&intent=submit_xls" enctype="multipart/form-data" method="POST">
 									<input type="hidden" name="action" value="import">
 								<table class="data">
 									<tr class="<?php echo $class; ?>">
 										<td>Excel bestand dienstregeling</td>
-										<td><input name="trainTableFileName" type="file" size="20M"></td>
+										<td><input name="trainTableFileName" type="file"></td>
 									</tr>
 								</table>
 									<input type="submit" name="FormAction" value="Laad gegevens">
